687b08ef8be2e05d40fde483ff5ff98fd1932de9
alexpinch <alexpinch@hotmail.com>
2023-01-28 19:06:59 -0800

    Update presentation_plots.R

--- presentation_plots.R
+++ presentation_plots.R
@@ -5,7 +5,7 @@ full_runs <- list.files("/Users/alexpinch/GitHub/private/jan30_presentation/data
 for (i in 1:files) {
   run <- (full_runs[i])
   typeof(run)
-  setwd(file.path("/Users/alexpinch/GitHub/private/jan30_presentation/data_101822/full_runs//",run))
+  setwd(file.path("/Users/alexpinch/GitHub/private/jan30_presentation/data_101822/full_runs/",run))
   run_data <- read.csv(file = paste(run,".csv",sep=""),skip=1,header=F) %>%
     rename(gen=V1,pop=V2,sample=V3,fitness=V4,inv_genotype=V5)
   run_data <- run_data %>% mutate(sim_run=run)
@@ -22,6 +22,7 @@ all_data %>%
   ggplot(.,aes(x=gen,y=mean_fitness,group=inv_genotype,color=inv_genotype)) +
   geom_smooth(method="loess") +
   facet_wrap(~pop)
+
 summarized_data <- all_data %>%
   mutate(fixed_fitness = case_when(inv_genotype == 2 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.1,
                                    inv_genotype == 2 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.1,
@@ -35,16 +36,17 @@ summarized_data <- all_data %>%
   mutate(qnt_90 = quantile(mean_fit, 0.9),
          qnt_10 = quantile(mean_fit, 0.1),
          mean_sim_fit = quantile(mean_fit, 0.9)) ## 0.5 = median
+
 summarized_data %>%
   ggplot(.) +
   geom_line(aes(x=gen,y=mean_sim_fit,group=inv_genotype,color=as.factor(inv_genotype))) +
   geom_ribbon(aes(x=gen,ymin=qnt_10,ymax=qnt_90,group=inv_genotype,fill=as.factor(inv_genotype)),alpha=0.2) # alpha = transparency
+
 #Allele frequency change over time
-genotype <- as_factor(inv_genotype)
 all_data %>%
   group_by(gen,pop,inv_genotype) %>%
   summarize(n=n()) %>%
   mutate(freq = n / sum(n)) %>%
-  ggplot(.,aes(x=gen,y=freq,color=genotype)) +
+  ggplot(.,aes(x=gen,y=freq,color=as_factor(inv_genotype))) +
   geom_line() +
   facet_wrap(~pop)
